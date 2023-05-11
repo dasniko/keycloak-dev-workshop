c63ec420e043739474cdd8d239ac7af6b4ed57bd
Niko Köbler <niko@n-k.de>
2023-05-11 19:46:10 +0200

    user storage repo

---
+++ src/main/java/de/keycloak/users/repo/FlintstoneUser.java
@@ -0,0 +1,32 @@
+package de.keycloak.users.repo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class FlintstoneUser {
+
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private boolean enabled;
+    private Long created;
+    private List<String> roles;
+
+    public FlintstoneUser(String email, String firstName, String lastName, boolean enabled, List<String> roles) {
+        this.username = email.substring(0, email.indexOf("@"));
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = firstName.toLowerCase();
+        this.enabled = enabled;
+        this.created = System.currentTimeMillis();
+        this.roles = roles;
+    }
+
+}
---
+++ src/main/java/de/keycloak/users/repo/FlintstonesRepository.java
@@ -0,0 +1,69 @@
+package de.keycloak.users.repo;
+
+import lombok.SneakyThrows;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class FlintstonesRepository {
+
+    private final List<FlintstoneUser> users = new ArrayList<>();
+
+    @SneakyThrows
+    public FlintstonesRepository() {
+        try (InputStream inputStream = Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("/flintstones.csv"))) {
+            List<String> lines = new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.toList());
+            lines.forEach(line -> {
+                String[] values = line.split(";");
+                users.add(
+                        new FlintstoneUser(values[0], values[1], values[2], Boolean.parseBoolean(values[3]), values.length > 4 ? List.of(values[4]) : null)
+                );
+            });
+        }
+    }
+
+    public List<FlintstoneUser> getAllUsers() {
+        return users;
+    }
+
+    public int getUsersCount() {
+        return users.size();
+    }
+
+    public FlintstoneUser findUserByUsernameOrEmail(String username) {
+        return users.stream()
+                .filter(user -> user.getUsername().equalsIgnoreCase(username) || user.getEmail().equalsIgnoreCase(username))
+                .findFirst().orElse(null);
+    }
+
+    public List<FlintstoneUser> findUsers(String query) {
+        return users.stream()
+                .filter(user -> query.equalsIgnoreCase("*") || user.getUsername().contains(query) || user.getEmail().contains(query))
+                .collect(Collectors.toList());
+    }
+
+    public boolean validateCredentials(String username, String password) {
+        return findUserByUsernameOrEmail(username).getPassword().equals(password);
+    }
+
+    public boolean updateCredentials(String username, String password) {
+        findUserByUsernameOrEmail(username).setPassword(password);
+        return true;
+    }
+
+    public void addUser(FlintstoneUser user) {
+        user.setCreated(System.currentTimeMillis());
+        user.setPassword(user.getFirstName().toLowerCase());
+        users.add(user);
+    }
+
+    public boolean removeUser(String username) {
+        return users.removeIf(p -> p.getUsername().equals(username));
+    }
+
+}
