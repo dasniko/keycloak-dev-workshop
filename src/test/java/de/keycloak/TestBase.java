33565f6b4186e825c13b7ea48d79fb27b3c5a56d
Niko Köbler <niko@n-k.de>
2024-03-23 11:28:37 +0100

    update testbase methods
    visibility & options

--- src/test/java/de/keycloak/TestBase.java
+++ src/test/java/de/keycloak/TestBase.java
@@ -8,14 +8,21 @@ import org.keycloak.utils.MediaType;
 
 import static io.restassured.RestAssured.given;
 
-@SuppressWarnings({"unused", "SameParameterValue"})
 public class TestBase {
 
-    ValidatableResponse requestToken(KeycloakContainer keycloak, String realm, String username, String password) {
+    public ValidatableResponse requestToken(KeycloakContainer keycloak, String realm, String username, String password) {
         return requestToken(keycloak, realm, username, password, 200);
     }
 
-    ValidatableResponse requestToken(KeycloakContainer keycloak, String realm, String username, String password, int expectedStatusCode) {
+    public ValidatableResponse requestToken(KeycloakContainer keycloak, String realm, String clientId, String username, String password) {
+        return requestToken(keycloak, realm, clientId, username, password, 200);
+    }
+
+    public ValidatableResponse requestToken(KeycloakContainer keycloak, String realm, String username, String password, int expectedStatusCode) {
+        return requestToken(keycloak, realm, KeycloakContainer.ADMIN_CLI_CLIENT, username, password, expectedStatusCode);
+    }
+
+    public ValidatableResponse requestToken(KeycloakContainer keycloak, String realm, String clientId, String username, String password, int expectedStatusCode) {
         String tokenEndpoint = getOpenIDConfiguration(keycloak, realm)
                 .extract().path("token_endpoint");
         return given()
@@ -23,13 +30,13 @@ public class TestBase {
                 .formParam(OAuth2Constants.USERNAME, username)
                 .formParam(OAuth2Constants.PASSWORD, password)
                 .formParam(OAuth2Constants.GRANT_TYPE, OAuth2Constants.PASSWORD)
-                .formParam(OAuth2Constants.CLIENT_ID, KeycloakContainer.ADMIN_CLI_CLIENT)
+                .formParam(OAuth2Constants.CLIENT_ID, clientId)
                 .formParam(OAuth2Constants.SCOPE, OAuth2Constants.SCOPE_OPENID)
                 .when().post(tokenEndpoint)
                 .then().statusCode(expectedStatusCode);
     }
 
-    ValidatableResponse getOpenIDConfiguration(KeycloakContainer keycloak, String realm) {
+    public ValidatableResponse getOpenIDConfiguration(KeycloakContainer keycloak, String realm) {
         return given().pathParam("realm-name", realm)
                 .when().get(keycloak.getAuthServerUrl() + ServiceUrlConstants.DISCOVERY_URL)
                 .then().statusCode(200);
