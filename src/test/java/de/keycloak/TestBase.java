964a9fb5e1e2f1e6f4fc1669f9847d643e7e1552
Niko Köbler <niko@n-k.de>
2024-11-13 17:25:55 +0000

    update TestBase class to provide more convenience methods

--- src/test/java/de/keycloak/TestBase.java
+++ src/test/java/de/keycloak/TestBase.java
@@ -1,28 +1,39 @@
 package de.keycloak;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dasniko.testcontainers.keycloak.KeycloakContainer;
 import io.restassured.response.ValidatableResponse;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.constants.ServiceUrlConstants;
 import org.keycloak.utils.MediaType;
 
+import java.io.IOException;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 
+@SuppressWarnings({"unused", "SameParameterValue", "UnusedReturnValue"})
 public class TestBase {
 
-    public ValidatableResponse requestToken(KeycloakContainer keycloak, String realm, String username, String password) {
+    protected final ObjectMapper mapper = new ObjectMapper();
+    protected final TypeReference<HashMap<String, Object>> mapTypeRef = new TypeReference<>() {};
+
+    protected ValidatableResponse requestToken(KeycloakContainer keycloak, String realm, String username, String password) {
         return requestToken(keycloak, realm, username, password, 200);
     }
 
-    public ValidatableResponse requestToken(KeycloakContainer keycloak, String realm, String clientId, String username, String password) {
+    protected ValidatableResponse requestToken(KeycloakContainer keycloak, String realm, String clientId, String username, String password) {
         return requestToken(keycloak, realm, clientId, username, password, 200);
     }
 
-    public ValidatableResponse requestToken(KeycloakContainer keycloak, String realm, String username, String password, int expectedStatusCode) {
+    protected ValidatableResponse requestToken(KeycloakContainer keycloak, String realm, String username, String password, int expectedStatusCode) {
         return requestToken(keycloak, realm, KeycloakContainer.ADMIN_CLI_CLIENT, username, password, expectedStatusCode);
     }
 
-    public ValidatableResponse requestToken(KeycloakContainer keycloak, String realm, String clientId, String username, String password, int expectedStatusCode) {
+    protected ValidatableResponse requestToken(KeycloakContainer keycloak, String realm, String clientId, String username, String password, int expectedStatusCode) {
         String tokenEndpoint = getOpenIDConfiguration(keycloak, realm)
                 .extract().path("token_endpoint");
         return given()
@@ -36,9 +47,14 @@ public class TestBase {
                 .then().statusCode(expectedStatusCode);
     }
 
-    public ValidatableResponse getOpenIDConfiguration(KeycloakContainer keycloak, String realm) {
+    protected ValidatableResponse getOpenIDConfiguration(KeycloakContainer keycloak, String realm) {
         return given().pathParam("realm-name", realm)
                 .when().get(keycloak.getAuthServerUrl() + ServiceUrlConstants.DISCOVERY_URL)
                 .then().statusCode(200);
+    }
+
+    protected Map<String, Object> parseToken(String token) throws IOException {
+        byte[] tokenPayload = Base64.getDecoder().decode(token.split("\\.")[1]);
+        return mapper.readValue(tokenPayload, mapTypeRef);
     }
 }
